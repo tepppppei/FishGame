09003dc4879e4c3d22bf0a746829e4c5c8cae2f0
tepppppei <macbook>
2015-12-10 08:32:27 +0900

    最初のコミット

---
+++ Assets/FishScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class FishScript : MonoBehaviour {
+    SardineCharacter sardineCharacter;
+    private bool startFlg = false;
+
+    // Use this for initialization
+    void Start () {
+        sardineCharacter = GetComponent<SardineCharacter> ();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!startFlg) {
+            startFlg = true;
+            StartCoroutine(randomMove());
+        }
+    }
+
+    IEnumerator randomMove() {
+        while (true) {
+            //進む方向
+            int direction = UnityEngine.Random.Range(0, 1);
+            //待機時間
+            float waitTime = UnityEngine.Random.Range(0.5f, 2.0f);
+
+            if (direction == 0) {
+                sardineCharacter.TurnLeft();
+            } else {
+                sardineCharacter.TurnRight();
+            }
+
+            sardineCharacter.MoveForward();
+
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerScript : MonoBehaviour {
+    private Vector3 touchPos;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            touchPos = Input.mousePosition;
+        } else if (Input.GetMouseButtonUp(0)) {
+            Vector3 releasePos = Input.mousePosition;
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            touchEffect(worldPos.x, worldPos.y);
+        }
+    }
+
+    void touchEffect(float x, float y) {
+        GameObject circlePrefab = (GameObject)Resources.Load("Prefab/Circle");
+        Instantiate (circlePrefab, new Vector3(x, y, -2), Quaternion.identity);
+    }
+}
